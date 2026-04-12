97b0c6107484468b8ca1e988b6b3326d5195d82a
Luan Alves <97049835+Luan029@users.noreply.github.com>
2026-04-12 08:38:49 -0300

    feat: updating user model

--- src/app/shared/models/user.model.ts
+++ src/app/shared/models/user.model.ts
@@ -2,4 +2,21 @@ export interface User {
     id: number;
     name: string;
     email: string;
+    cpf: string;
+    phone: string;
+    phoneType: PhoneType;
+}
+
+export type PhoneType = 'CELULAR' | 'RESIDENCIAL' | 'COMERCIAL';
+
+export interface UserFormData {
+    email: string;
+    name: string;
+    cpf: string;
+    phone: string;
+    phoneType: PhoneType;
+}
+
+export interface UserDialogData {
+    user: User | null;
 }
